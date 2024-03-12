aa23b1c9d99bace0eb9d45ef8d778c2622c4b5ab
macie <01178676@pw.edu.pl>
2024-03-12 17:14:17 +0100

    Cowboy class added

--- src/Cowboy.java
+++ src/Cowboy.java
@@ -1,2 +1,13 @@
-public class Cowboy extends {
+import javax.swing.*;
+
+public class Cowboy extends JComponent {
+    private int xposition;
+    private int yposition;
+
+    private Cowboy(int xposition,int yposition){
+        setVisible(true);
+        setBounds(xposition,yposition,75,75);
+
+    }
+
 }
